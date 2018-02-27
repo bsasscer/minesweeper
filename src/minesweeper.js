f73af7998da4c87ac63b809a2a7e045cd81089e5
bsasscer <smithbi.maricela@gmail.com>
2018-02-27 15:00:30 -0800

    build out first part of minesweeper project

---
+++ src/minesweeper.js
@@ -0,0 +1,7 @@
+const blankLine = '  |   |  ';
+const guessLine = '1 |   |  ';
+const bombLine  = '  | B |  ';
+
+console.log('This is what an empty board would look like: ' + '\n' + blankLine + '\n' + blankLine + '\n' + blankLine);
+
+console.log('This is what a board with a guess and a bomb on it would look like: ' + '\n' + guessLine + '\n' + bombLine + '\n' + blankLine);
